d2e9949f31ed4891371edee98c888c686cca5262
Jenn <jennogden95@gmail.com>
2019-04-11 13:18:24 -0700

    FIX: Now renders Pug templates and routes for POST req

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('It works!');
+  res.render('form', { title: 'Bandersguru Home Screen' });
+});
+
+router.post('/', (req, res) => {
+  res.render('form', { title: 'Bandersguru Home Screen' });
 });
 
 module.exports = router;
